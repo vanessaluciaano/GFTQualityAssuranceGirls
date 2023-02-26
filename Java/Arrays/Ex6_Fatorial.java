1c87bf3319113884003f25651a344bc47879efed
vanessaluciaano <vanessa.lucianav@gmail.com>
2023-02-26 11:33:23 -0300

    Calcula o fatorial de um número inteiro

---
+++ Java/Arrays/Ex6_Fatorial.java
@@ -0,0 +1,20 @@
+// Calcula o fatorial de um número inteiro fornecido pelo usuário
+
+import java.util.Scanner;
+
+public class Ex6_Fatorial {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Qual número deseja ver o fatorial?");
+        int fatorial = scan.nextInt();
+
+        int multiplicacao = 1;
+
+        System.out.print(fatorial +"! = ");
+        for(int i = fatorial; i >= 1; i --) {
+            multiplicacao = multiplicacao * i;
+        }
+        System.out.println(multiplicacao);
+        }
+    }
