d0a3385fdddacc569605b010812a434af25bcd21
vanessaluciaano <vanessa.lucianav@gmail.com>
2023-02-26 10:39:33 -0300

    Recebe 5 numeros e exibe o maior e a media

---
+++ Java/Arrays/Ex3_MaiorMedia.java
@@ -0,0 +1,30 @@
+/* Lê 55 números e informa o maior número
+e a média desses números.
+ */
+
+import java.util.Scanner;
+
+public class Ex3_MaiorMedia {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int numero;
+        int maior = 0;
+        int soma = 0;
+
+        int count = 0;
+        do {
+            System.out.println("Número: ");
+            numero = scan.nextInt();
+
+            soma = soma + numero;
+            if (numero > maior) maior = numero;
+
+            count = count + 1;
+
+        } while (count < 5);
+
+        System.out.println("Maior: " + maior);
+        System.out.println("Média: " + (soma/5));
+    }
+}
